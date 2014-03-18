194a09321c527e5d53f8086cac0ac0dc9ad32f97
Phillipe Gustavo Ferreira <phillipe.gustavo@gmail.com>
2014-03-17 21:37:09 -0300

    Projeto inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Acad::Application.config.secret_key_base = '1553ebd93d09c95009d18512cc606edd33909a51bd1ace9680068940d9d411e5fe2c8782df172374a2323bb49123640e66133025fcbbdf9be352579da87eb8c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Acad::Application.config.session_store :cookie_store, key: '_acad_session'
